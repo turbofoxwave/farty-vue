cdd1504bcdf8809436e4e58d81aac1d54de5ea58
mike benjamin <turbofoxwave@gmail.com>
2019-03-12 01:27:29 -0400

    simple drag/drop of food items

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,14 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">Farty Vue</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
         flat
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      >        
       </v-btn>
     </v-toolbar>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,147 +1,101 @@
 <template>
   <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
+    <v-layout text-xs-center wrap>
       <v-flex xs12>
+        <!-- <v-img :src="require('../assets/logo.svg')" class="my-3" contain height="200"></v-img> -->
+        <draggable class="mouth" v-model="inMouth" :group="{name: 'eating', pull: false,put:true}" @change="onChange">
         <v-img
-          :src="require('../assets/logo.svg')"
+          v-if="isChewing === false"
+          :src="require('../assets/still.png')"
           class="my-3"
           contain
           height="200"
+          width="200"
         ></v-img>
+        <v-img
+          v-if="isChewing === true"
+          :src="require('../assets/chewing.gif')"
+          class="my-3"
+          contain
+          height="200"
+          width="200"
+        ></v-img>
+        </draggable>
+        
       </v-flex>
 
       <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
+        <v-content>
+          <h2>foods</h2>
+        </v-content>
+        <draggable class="foodBag" v-model="foodBag" :group="{ name: 'food', pull:'clone', put: false}">
+          <v-img class="foodItem" v-for="element in foodBag" :key="element.name" :src="element.file" contain height="64" width="64"></v-img>
+          <!-- <div v-for="element in foodBag" :key="element.id">{{element.name}}</div> -->
+        </draggable>
       </v-flex>
 
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
+      <v-flex mb-5 xs12></v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
+import draggable from 'vuedraggable'
+import chicken from "../assets/chicken.svg"
+import cheese from "../assets/cheese.svg"
+import hamburger from "../assets/hamburger.svg"
 
-      ]
-    })
+export default {
+  data: () => ({
+    isChewing: false,
+    foodBag: [
+      { name: "chicken", file: chicken },
+      { name: "hamburger", file: hamburger },
+      { name: "cheese", file: cheese },
+    ],
+    inMouth:[],
+  }),
+  components: {
+    draggable
+  },
+  methods:{
+    onChange: function(event){
+      //todo:add food to digestion system.
+      //this.inMouth.pop()
+
+      this.isChewing = true
+      setTimeout(()=>{
+        this.isChewing = false
+      }, 3000)
+
+      console.log(event)
+    },
+    onAdd: function(event){
+      //this.inMouth.pop()
+      console.log(event)
+    },
+	  onClone: function (/**Event*/evt) {
+      var origEl = evt.item;
+      var cloneEl = evt.clone;
+      console.log(origEl)
+      console.log(cloneEl)
+    },
   }
+}
 </script>
 
 <style>
-
+.foodItem{
+  display:flex;
+  float:left;
+}
+.foodBag{
+  display:inline-flex;
+}
+.mouth{
+  display:inline-flex;
+}
+.sortable-ghost{
+  display:none;
+}
 </style>
