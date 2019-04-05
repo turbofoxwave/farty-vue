aa7477ca3529c88e7e0472b19c0809b9e8fbf802
mike benjamin <turbofoxwave@gmail.com>
2019-04-04 23:12:01 -0400

    switching to patrick

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -5,16 +5,23 @@ import thudMp3 from '../assets/thud.mp3'
 import thudOgg from '../assets/thud.ogg'
 import pete from '../assets/pete.png'
 
+import patrick from '../assets/patrick-base_frame_0.png'
+import patrickEyes from '../assets/patrick-eyes.png'
+import patrickMouth from '../assets/patrick-mouth-chomp.png'
+
 export default class BootScene extends Scene {
   constructor () {
     super({ key: 'BootScene' })
   }
 
   preload () {
-    this.load.image('sky', sky)
-    this.load.image('bomb', bomb)
-    this.load.audio('thud', [thudMp3, thudOgg])
-    this.load.spritesheet('chomp', pete, { frameWidth: 313, frameHeight: 451, endFrame: 23 });
+    // this.load.image('sky', sky)
+    // this.load.image('bomb', bomb)
+    // this.load.audio('thud', [thudMp3, thudOgg])
+    this.load.spritesheet('patrick', patrick, { frameWidth: 540, frameHeight:443, endFrame :0})
+    this.load.spritesheet('patrick-mouth', patrickMouth, { frameWidth: 256, frameHeight: 128, endFrame: 5 });
+    this.load.spritesheet('patrick-eyes', patrickEyes, { frameWidth: 256, frameHeight: 64, endFrame: 28 });
+    // this.load.spritesheet('chomp', pete, { frameWidth: 313, frameHeight: 451, endFrame: 23 });
 
   }
 
--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -7,60 +7,119 @@ export default class PlayScene extends Scene {
   }
 
   create () {
-    this.load.spritesheet('chomp', 'assets/pete.png', { frameWidth: 313, frameHeight: 451, endFrame: 23 });
+    // this.load.spritesheet('chomp', 'assets/pete.png', { frameWidth: 313, frameHeight: 451, endFrame: 23 });
+
+    // this.load.spritesheet('chomp', 'assets/pete.png', { frameWidth: 313, frameHeight: 451, endFrame: 23 });
 
 
     //this.add.image(400, 300, 'sky')
 
-    const bomb = this.physics.add.image(400, 200, 'bomb')
-    bomb.setCollideWorldBounds(true)
-    bomb.body.onWorldBounds = true // enable worldbounds collision event
-    bomb.setBounce(1)
-    bomb.setVelocity(200, 20)
+    // const bomb = this.physics.add.image(400, 200, 'bomb')
+    // bomb.setCollideWorldBounds(true)
+    // bomb.body.onWorldBounds = true // enable worldbounds collision event
+    // bomb.setBounce(1)
+    // bomb.setVelocity(200, 20)
 
-    this.sound.add('thud')
-    this.physics.world.on('worldbounds', () => {
-      this.sound.play('thud', { volume: 0.75 })
-    })
+    // this.sound.add('thud')
+    // this.physics.world.on('worldbounds', () => {
+    //   this.sound.play('thud', { volume: 0.75 })
+    // })
 
+    let patrick = this.add.sprite(150,100,'patrick',0)
+    patrick.scaleX = .45;
+    patrick.scaleY = .45;
+    let patrickEyes = this.add.sprite(215,50, 'patrick-eyes', 0)
+    patrickEyes.scaleX = .25
+    patrickEyes.scaleY = .25
+    let patrickMouth = this.add.sprite(208,95, 'patrick-mouth',0)
+    patrickMouth.scaleX = .2
+    patrickMouth.scaleY = .2
+
+    patrick.setInteractive()
+
+
+
+    //setup animations
     let configChomp = {
-      key: 'pete-chomp',
-      frames: this.anims.generateFrameNumbers('chomp', { start: 0, end: 24, first: 0 }),
+      key: 'patrick-chomp',
+      frames: this.anims.generateFrameNumbers('patrick-mouth', { start: 0, end: 4, first: 4 }),
+      frameRate: 30,
+      repeat: 5,
+      repeatDelay: 2
+    };
+    this.anims.create(configChomp);
+
+    let configMouthClosed = {
+      key: 'patrick-mouth-closed',
+      frames: this.anims.generateFrameNumbers('patrick-mouth', { start: 0, end: 0, first: 0 }),
+      frameRate: 30,
+      repeat: -1,
+      repeatDelay: 2
+    };
+    this.anims.create(configMouthClosed);
+
+
+    let googlyEyes = {
+      key: 'patrick-eyes-googly',
+      frames: this.anims.generateFrameNumbers('patrick-eyes', { start: 0, end: 28, first: 0 }),
       frameRate: 60,
       repeat: 1,
       repeatDelay: 2
     };
-
-    this.anims.create(configChomp);
-
-    var configStill = {
-      key: 'pete-still',
-      frames: this.anims.generateFrameNumbers('chomp', { start: 0, end: 0, first: 0 }),
-      frameRate: 30,
-      repeat: 0,
-      repeatDelay: 2
-    };
-
-    this.anims.create(configStill);
+    this.anims.create(googlyEyes);
 
 
-    let peteChomp = this.add.sprite(100, 100, 'chomp', 0);
-    peteChomp.setInteractive()
-    peteChomp.scaleX = .5;
-    peteChomp.scaleY = .5;
+    patrick.on('pointerdown', function(pointer){
+      patrickMouth.anims.delayedPlay(1,"patrick-chomp")
+      .anims.chain('patrick-mouth-closed')
+      // .on('animationcomplete', function(animation, frame){
+      //   peteChomp.anims.(1,"pete-still")
+      // },this)
 
-    peteChomp.on('chomp', function(){
-      if(peteChomp.anims.isPlaying === true && peteChomp.anims.getCurrentKey() === 'pete-chomp') return;
+      patrickEyes.anims.delayedPlay(1,"patrick-eyes-googly")
 
-      peteChomp.anims.delayedPlay(1,"pete-chomp")
-      .on('animationcomplete', function(animation, frame){
-        peteChomp.anims.delayedPlay(1,"pete-still")
-      },this)
     })
 
-    peteChomp.on('pointerdown', function(pointer){
-      peteChomp.emit('chomp')
-    })
+
+    // let configChomp = {
+    //   key: 'pete-chomp',
+    //   frames: this.anims.generateFrameNumbers('chomp', { start: 0, end: 24, first: 0 }),
+    //   frameRate: 60,
+    //   repeat: 1,
+    //   repeatDelay: 2
+    // };
+
+    // this.anims.create(configChomp);
+
+    // var configStill = {
+    //   key: 'pete-still',
+    //   frames: this.anims.generateFrameNumbers('chomp', { start: 0, end: 0, first: 0 }),
+    //   frameRate: 30,
+    //   repeat: 0,
+    //   repeatDelay: 2
+    // };
+
+    // this.anims.create(configStill);
+
+
+    // let peteChomp = this.add.sprite(100, 100, 'chomp', 0);
+    // peteChomp.setInteractive()
+    // peteChomp.scaleX = .5;
+    // peteChomp.scaleY = .5;
+
+    // peteChomp.on('chomp', function(){
+    //   if(peteChomp.anims.isPlaying === true && peteChomp.anims.getCurrentKey() === 'pete-chomp') return;
+
+    //   peteChomp.anims.delayedPlay(1,"pete-chomp")
+    //   .on('animationcomplete', function(animation, frame){
+    //     peteChomp.anims.delayedPlay(1,"pete-still")
+    //   },this)
+    // })
+
+    // peteChomp.on('pointerdown', function(pointer){
+    //   peteChomp.emit('chomp')
+    // })
+
     // let peteStill = this.add.sprite(100, 100, 'still', 4);
     // peteStill.scaleX = .5;
     // peteStill.scaleY = .5;
